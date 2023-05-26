07e06f186639d16107120a8ebe1646cd562a2903
darrshantg <114505879+darrshantg@users.noreply.github.com>
2023-05-26 19:15:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
+const app = express();
+
+connectDb();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts",require("./routes/contactRoutes"));
+app.use("/api/users",require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}`);
+});
